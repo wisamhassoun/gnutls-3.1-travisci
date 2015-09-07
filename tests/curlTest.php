5a98152cfd2f8da82af0da95fa7f41f30cac475e
Wisam Hassoun <wiha@admins-MacBook-Pro-9.local>
2015-09-07 09:57:03 +0200

    adding tests

--- tests/curlTest.php
+++ tests/curlTest.php
@@ -6,9 +6,4 @@ class curlTest extends PHPUnit_Framework_TestCase {
 		$cv=curl_version();
 		$this->assertTrue($cv['ssl_version'] == "GnuTLS/3.1.28");
     }
-
-    public function wisamVersion() {
-    		return "kalleanka";
-        }
-
 }
